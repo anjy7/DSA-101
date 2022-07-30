daee725ff94b88d061174084c63169851167b3d3
Anjy Gupta <92802904+anjy7@users.noreply.github.com>
2022-07-31 01:02:31 +0530

    Time: 18 ms (29.78%), Space: 44.1 MB (76.70%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -3,24 +3,17 @@ class Solution {
          if (x<0){
             return false;
         }
-        
-        int count = 0;
+
         int temp = x;
         int n=0, m = 0;
         
-       
-        
-        while (temp !=0) {
+        while (temp != 0) {
             n = temp % 10;
             temp = temp /10;
             m = m * 10 + n;
         }
-        
-        if(x == m){
-            return true;
-        }else{
-            return false;
-        }
+    
+        return x==m;
         
     }
 }
